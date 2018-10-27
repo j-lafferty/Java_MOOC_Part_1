feb22727295282ff2c469ab3e9896d5b68f52929
James Lafferty <james@j-lafferty.net>
2018-10-26 21:07:01 -0700

    Add solution

--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -7,5 +7,15 @@ public class Divider {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user.
+        System.out.print("Type a number: ");
+        int firstNumb = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type another number: ");
+        int secondNumb = Integer.parseInt(reader.nextLine());
+        
+        double division = (1.0 * firstNumb) / secondNumb;
+        
+        System.out.println("Division: " + firstNumb + " / " + secondNumb + 
+                " = " + division);
     }
 }
