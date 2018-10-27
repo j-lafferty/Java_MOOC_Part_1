d686bfac619947898c9077270602fdbde70b6c03
James Lafferty <james@j-lafferty.net>
2018-10-26 20:52:11 -0700

    Add solution

--- week1-008.Adder/src/Adder.java
+++ week1-008.Adder/src/Adder.java
@@ -7,5 +7,14 @@ public class Adder {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user
+        System.out.print("Type a number: ");
+        int firstNumb = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type another number: ");
+        int secondNumb = Integer.parseInt(reader.nextLine());
+        
+        int sum = firstNumb + secondNumb;
+        
+        System.out.println("Sum of numbers: " + sum);
     }
 }
