4e7822c2d5ccb40abdfb3e38f73711617a6229ed
James Lafferty <james@j-lafferty.net>
2018-10-26 21:31:57 -0700

    Add solution

--- week1-010.CircleCircumference/src/Circumference.java
+++ week1-010.CircleCircumference/src/Circumference.java
@@ -6,5 +6,12 @@ public class Circumference {
         Scanner reader = new Scanner(System.in);
 
         // Program your solution here 
+        System.out.print("Type the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        
+        double circumference = 2 * Math.PI * radius;
+        
+        System.out.println("Circumference of the circle: " + circumference);
+        
     }
 }
