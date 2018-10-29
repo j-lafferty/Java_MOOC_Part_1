da59fb1dfde24d18e89669d87c4edf3d64317de1
James Lafferty <james@j-lafferty.net>
2018-10-28 20:06:15 -0700

    Add solution

--- week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -7,5 +7,21 @@ public class SumOfTheAges {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here
+        System.out.print("Type your name: ");
+        String name1 = reader.nextLine();
+        
+        System.out.print("Type your age: ");
+        int age1 = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type your name: ");
+        String name2 = reader.nextLine();
+        
+        System.out.print("Type your age: ");
+        int age2 = Integer.parseInt(reader.nextLine());
+        
+        int ageSum = age1 + age2;
+        
+        System.out.println(name1 + " and " + name2 + " are " + ageSum + 
+                " years old in total.");
     }
 }
