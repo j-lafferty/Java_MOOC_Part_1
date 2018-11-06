3080f431f6ff90d7c97c45ffbaf701e077f04247
James Lafferty <james@j-lafferty.net>
2018-11-06 12:06:58 -0800

    Add solution

--- week1-020.Usernames/src/Usernames.java
+++ week1-020.Usernames/src/Usernames.java
@@ -6,5 +6,23 @@ public class Usernames {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type your username: ");
+        String user = reader.nextLine();
+
+        System.out.println("Type your password: ");
+        String pass = reader.nextLine();
+
+        String user1 = "alex";
+        String pass1 = "mightyducks";
+
+        String user2 = "emily";
+        String pass2 = "cat";
+
+        if ((user.equals(user1) && pass.equals(pass1)) || (user.equals(user2) && pass.equals(pass2))) {
+            System.out.println("You are now logged into the system!");
+        } else {
+            System.out.println("Your username or password was invalid!");
+        }
+
     }
 }
