9c3121d234827c7665140a643ac28fd3ebd1ef29
James Lafferty <james@j-lafferty.net>
2018-11-07 11:58:00 -0800

    Add solution

--- week1-022.Password/src/Password.java
+++ week1-022.Password/src/Password.java
@@ -8,5 +8,19 @@ public class Password {
         String password = "carrot"; // Use carrot as password when running tests.
 
         // Write your code here
+
+        while (true) {
+            System.out.println("Type the password: ");
+            String input = reader.nextLine();
+
+            if (input.equals(password)) {
+                break;
+            }
+            System.out.println("Wrong!");
+        }
+
+        System.out.println("Right!");
+        System.out.println("The secret is: jryy qbar!");
+
     }
 }
