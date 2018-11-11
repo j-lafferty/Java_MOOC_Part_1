e1d963f687ba4de3298d00fda63f6e6d84c24713
James Lafferty <james@j-lafferty.net>
2018-11-10 21:56:36 -0800

    Add solution

--- week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
+++ week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
@@ -7,6 +7,18 @@ public class LowerLimitAndUpperLimit {
         Scanner reader = new Scanner(System.in);
 
         // write your code here
+        System.out.println("First: ");
+        int first = Integer.parseInt(reader.nextLine());
+        
+        System.out.println("Second: ");
+        int second = Integer.parseInt(reader.nextLine());
+        
+        int i = first;
+        
+        while (i <= second) {
+            System.out.println(i);
+            i++;
+        }
 
     }
 }
