8bf462ab720c5667c298bc72d478831ac100dd50
James Lafferty <james@j-lafferty.net>
2018-11-10 21:50:09 -0800

    add solution

--- week2-029.EvenNumbers/src/EvenNumbers.java
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -3,5 +3,11 @@ public class EvenNumbers {
 
     public static void main(String[] args) {
         // Write your code here
+        int number = 0;
+        
+        while (number < 100) {
+            number += 2;
+            System.out.println(number);
+        }
     }
 }
