a41629aa13485859f34e221ec559d38e6073ef94
James Lafferty <james@j-lafferty.net>
2018-11-11 21:02:42 -0800

    Add solution

--- week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -6,5 +6,22 @@ public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
+        System.out.println("First: ");
+        int first = Integer.parseInt(reader.nextLine());
+        
+        System.out.println("Last: ");
+        int last = Integer.parseInt(reader.nextLine());
+        
+        int result = first;
+        int i = first;
+        
+        while (i < last) {
+            i++;
+            result += i;
+        }
+        
+        System.out.println("The sum is " + result);
+        
+        
     }
 }
