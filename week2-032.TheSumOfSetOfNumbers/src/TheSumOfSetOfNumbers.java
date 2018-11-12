c55a9937790468584b65859f132fc5d4cf37a877
James Lafferty <james@j-lafferty.net>
2018-11-11 20:58:31 -0800

    Add solution

--- week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -5,6 +5,19 @@ public class TheSumOfSetOfNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Until what? ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        int result = 0;
+        int i = 0;
+        
+        while (i <= number) {
+            result += i;
+            i++;
+        }
+        
+        System.out.println("Sum is " + result);
 
     }
 }
