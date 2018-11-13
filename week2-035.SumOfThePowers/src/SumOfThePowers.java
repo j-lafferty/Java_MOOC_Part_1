80d2d416796bc7cf10fe51334ae3ab2c274a2d77
James Lafferty <james@j-lafferty.net>
2018-11-12 21:11:40 -0800

    Add solution

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,18 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        int result = 0;
+        int i = 0;
+        
+        while (i <= number) {
+            result += (int)Math.pow(2, i);
+            i++;
+        }
+        System.out.println("The result is " + result);
 
     }
 }
