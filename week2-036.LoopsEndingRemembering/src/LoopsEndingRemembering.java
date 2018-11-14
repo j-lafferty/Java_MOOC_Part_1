4b45da8a09bd45a40bdadb2d10dc36340beb24f5
James Lafferty <james@j-lafferty.net>
2018-11-13 21:02:02 -0800

    Fix average to be a floating point number

--- week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -22,7 +22,7 @@ public class LoopsEndingRemembering {
                 System.out.println("Thank you and see you later!");
                 System.out.println("The sum is " + sum);
                 System.out.println("How many numbers: " + i);
-                System.out.println("Average: " + (sum / i));
+                System.out.println("Average: " + (sum / (i * 1.0)));
                 System.out.println("Even numbers: " + even);
                 System.out.println("Odd numbers: " + odd);
                 break;
