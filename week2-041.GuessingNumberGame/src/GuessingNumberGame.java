e8c2ce4205ddc4c6d17d02170e46aaa395e1f542
James Lafferty <james@j-lafferty.net>
2019-03-31 15:18:48 -0700

    Add solution

--- week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -9,8 +9,25 @@ public class GuessingNumberGame {
         int numberDrawn = drawNumber();
 
         // program your solution here. Do not touch the above lines!
+        boolean number = false;
+        int i = 0;
         
+        while (number == false) {
+            System.out.print("Guess a number: ");
+            int userNumber = Integer.parseInt(reader.nextLine());
+            i++;
+            
+            if (userNumber > numberDrawn) {
+                System.out.println("The number is lesser, guesses made: " + i);
+            } else if (userNumber < numberDrawn) {
+                System.out.println("The number is greater, guesses made: " + i);
+            } else {
+                number = true;
+            }
+        }
+        System.out.println("Congratulations, your number is correct! Guesses made: " + i);
     }
+
 
     // DO NOT MODIFY THIS!
     private static int drawNumber() {
