2004dfbf1fe1659b5bf2b97947b795a585a04510
James Lafferty <james@j-lafferty.net>
2019-04-02 17:43:09 -0700

    Add solution

--- week3-047.LengthOfName/src/LengthOfName.java
+++ week3-047.LengthOfName/src/LengthOfName.java
@@ -5,9 +5,18 @@ public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // call your method from here
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+
+        int characters = calculateCharacters(name);
+
+        System.out.println("Number of characters: " + characters);
     }
     
     // do here the method
     // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text) {
+        return text.length();
+    }
     
 }
