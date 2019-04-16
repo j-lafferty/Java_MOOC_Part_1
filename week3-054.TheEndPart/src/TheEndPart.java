e9b86e489f339ff8044bd4a818677a1f4b5e1743
James Lafferty <james@j-lafferty.net>
2019-04-16 07:06:12 -0700

    Add solution

--- week3-054.TheEndPart/src/TheEndPart.java
+++ week3-054.TheEndPart/src/TheEndPart.java
@@ -3,5 +3,17 @@ import java.util.Scanner;
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+
+        System.out.print("Length of the end part: ");
+        int length = Integer.parseInt(reader.nextLine());
+        
+        String endpart = word.substring(word.length() - length);
+        
+        
+        System.out.println("Result: " + endpart);
+        
     }
 }
