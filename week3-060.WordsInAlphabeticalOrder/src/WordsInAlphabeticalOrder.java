ecee577feffbc9a1f9a98e01a6563b478d691d4e
James Lafferty <james@j-lafferty.net>
2019-04-17 14:53:33 -0700

    Add solution

--- week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -8,5 +8,25 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        System.out.println("Type a word: ");
+        String word = reader.nextLine();
+
+        while (word.isEmpty() == false) {
+            words.add(word);
+
+            System.out.println("Type a word: ");
+            word = reader.nextLine();
+
+        }
+
+        Collections.sort(words);
+
+        System.out.println("You typed the following words: ");
+        for (String item : words) {
+            System.out.println(item);
+        }
     }
 }
