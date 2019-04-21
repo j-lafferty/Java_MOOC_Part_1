f13d5b264ab604488aabd0d45ed657f161f015e1
James Lafferty <james@j-lafferty.net>
2019-04-21 11:48:55 -0700

    Add solution

--- week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
+++ week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -4,6 +4,14 @@ public class Accounts {
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account matt = new Account("Matt's account", 1000.0);
+        Account myAccount = new Account("My account", 0.0);
+        
+        matt.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        
+        System.out.println(matt.toString());
+        System.out.println(myAccount.toString());
     }
 
 }
