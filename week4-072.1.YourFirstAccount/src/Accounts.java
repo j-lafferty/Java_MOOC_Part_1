6aabcf8e5ed2b33ca7b7383014d5f34ca43225df
James Lafferty <james@j-lafferty.net>
2019-04-21 11:44:58 -0700

    Add solution

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -5,7 +5,11 @@ public class Accounts {
 
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
-        // write your code here
+        // write your code here        
+        
+        Account account = new Account("Account", 100.0);
+        account.deposit(20.0);
+        System.out.println(account.toString());
     }
 
 }
